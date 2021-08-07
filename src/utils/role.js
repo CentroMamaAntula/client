ed749cd379aaabe710283e8ccfaad1994e4a1315
CentroMamaAntula <85749037+CentroMamaAntula@users.noreply.github.com>
2021-08-07 08:43:28 -0300

    role

--- src/utils/role.js
+++ src/utils/role.js
@@ -1,7 +1,7 @@
-export const MEDICO = 1;
-export const ENFERMERO = 2;
-export const SALUD_MENTAL = 3;
-export const RAYOS = 4;
-export const KINESILOGIA = 5;
-export const NUTRICION = 6;
-export const LABORATORIO = 7;
+export const MEDICO = '$2y$12$9dhFVEGXN2aZh0tz4GC8g.f.W3rwY4xnwb6DieaAoNYti.QwnRjDa';
+export const ENFERMERO = '$2y$12$8YKyxjJVXauC8IW9DCXStuQYzWvR26YIFThVLQeMVrtzbGKmd1x1W';
+export const SALUD_MENTAL = '$2y$12$28zhL1cEJI.smWIKRzaiaOJ0RH.RUlyr9QY3NU3jwsrL6lsFYP8Ee';
+export const RAYOS = '$2y$12$glxjJoqa7wZUakG0MUKTPuqMmTa/A5c9SzW/74LRsPtiyOPy5oHve';
+export const KINESILOGIA = '$2y$12$cQmOV2mQUYH.3zfpyn8hc.9A9Znj4IXD2g.4E2OWmAbBXRs4GG7aW';
+export const NUTRICION = '$2y$12$gp.IURMdULQqjehUZXCLCeBm0L0HKoPufWsujkgXx1THRk8VQgKly';
+export const LABORATORIO = '$2y$12$/dMcIn8Wz09qF6z6vrN1/egjrDN9jvTGEzSVZT9o/ABjqs1dqMHx2';
